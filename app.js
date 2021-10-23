0158aafbcd3cc1641057cec44245a446e2224581
jyanev01 <jordanyanev@gmail.com>
2021-10-23 13:52:19 -0400

    feels like a good spot to commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// var message = 'Hello Node!';
+// var sum = 5 +3;
+
+
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
