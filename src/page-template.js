89d036d8ec88377b1e11a303fcb2e7766ca31c4e
jyanev01 <jordanyanev@gmail.com>
2021-10-25 18:49:59 -0400

    full generation of HTML webpage

--- src/page-template.js
+++ src/page-template.js
@@ -12,19 +12,53 @@ const generateAbout = aboutText => {
   `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
   return `
     <section class="my-3" id="portfolio">
       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
       <div class="flex-row justify-space-between">
-        <!-- Leaving this emply as we'll dunamically insert project HTML here -->
+      ${projectsArr
+      .filter(({ feature }) => feature)
+      .map(({ name, description, languages, link }) => {
+        return `
+        <div class="col-12 mb-2 bg-dark text-light p-3">
+          <h3 class="portfolio-itme-title text-light">${name}</h3>
+          <h5 class="portfolio-languages">
+            Built With:
+            ${languages.map(language => language).join(',')}
+          </h5>
+          <p> ${description}</p>
+          <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+        `;
+      })
+      .join('')}
+
+      ${projectsArr
+      .filter(({ feature }) => !feature)
+      .map(({ name, description, languages, link }) => {
+        return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class= "portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(',')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+            `;
+      })
+      .join('')}
       </div>
     </section>
   `;
 };
 
+// export function to generate entire page
 module.exports = templateData => {
-  console.log(templateData);
+  // console.log(templateData);
   // destruct projects and about data from templateData based on their property key names
   const { projects, about, ...header } = templateData;
 
@@ -61,6 +95,6 @@ module.exports = templateData => {
     </footer>
   </body>
   </html>
-    `;
+  `;
 };
 
